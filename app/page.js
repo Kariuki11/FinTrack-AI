ed2cab80f450f8b190f821ad9cfbcb36017f78d4
Kariuki11 <irungukenneth433@gmail.com>
2025-01-09 21:00:36 +0300

    New Updates on the header, Layout file

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import {Button} from "@/components/ui/button"
 
 export default function Home() {
-  return <Button variant="destructive">Subscribe to RoadSideCoder</Button>
+  // return <Button variant="destructive">Subscribe to RoadSideCoder</Button>
 }
