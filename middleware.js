bedb2f5172831604c18345523a0107721689d83e
Kariuki11 <irungukenneth433@gmail.com>
2025-01-09 19:53:28 +0300

    New Updates on Authentication Part

--- middleware.js
+++ middleware.js
@@ -4,8 +4,16 @@ const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
-])
-export default clerkMiddleware();
+]);
+export default clerkMiddleware(async (auth, req)=>{
+  const { userId } = await auth();
+
+  if(!userId && isProtectedRoute(req)){
+    const { redirectToSignIn } = await auth();
+
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
