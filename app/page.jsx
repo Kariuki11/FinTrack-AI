a601c814849efe754fae207fd56d3cecd73e2da3
Kariuki11 <irungukenneth433@gmail.com>
2025-01-11 16:30:26 +0300

    New changes on page.jsx, landing.js

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 import HeroSection from "@/components/hero";
 import {Button} from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card";
-import { featuresData, statsData } from "@/data/landing";
+import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -21,7 +22,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
+      <section className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="3xl font-bold text-center mb-12">
             What You need to manage Your Finances.
@@ -39,6 +40,54 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="py-20 bg-blue-100">
+        <div className="container mx-auto px-4">
+          <h2 className="3xl font-bold text-center mb-17">
+            How the site works.
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {howItWorksData.map((step, index)=>(
+              <div key={index} className="text-center">
+                <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6">{step.icon}</div>
+                <h3 className="text-xl font-bold mb-5">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="3xl font-bold text-center mb-12">
+            Tstimonials
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {testimonialsData.map((testimonial, index)=>(
+              <Card key={index} className="p-6">
+                <CardContent className="space-y-4 pt-4">
+                    <div className="flex items-center mb-4">
+                        <Image
+                          src={testimonial.image}
+                          alt={testimonial.name}
+                          width={40}
+                          height={40}
+                          className="rounded-full"
+                        />
+                        <div className="ml-4">
+                          <div className="font-semibold">{testimonial.name}</div>
+                          <div className="text-sm text-gray-600">{testimonial.role}</div>
+                        </div>
+                    </div>
+                    <p className="text-gray-600">{testimonial.quote}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
   </div>
   );
 }
