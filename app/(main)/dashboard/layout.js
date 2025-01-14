3b75d9cf23d61a6ec9b43d96657ad5ba953e82b0
Kariuki11 <irungukenneth433@gmail.com>
2025-01-15 02:11:13 +0300

    Updates on layout page on the dashboard section

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import Dashboard from './page';
+import { BarLoader } from 'react-spinners';
 
 const DashboardLayout = () => {
   return (
@@ -7,7 +8,8 @@ const DashboardLayout = () => {
         <h1 className="text-7xl font-bold gradient-title mb-5">tableau de bord</h1>
 
         {/* Dashboard Page */}
-        <Suspense fallback={}>
+        <Suspense
+            fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}>
             <Dashboard />
         </Suspense>
     </div>
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div className="px-5">
-        <h1 className="text-7xl font-bold gradient-title mb-5">tableau de bord</h1>
+        {/* Budget Progress */}
+
+        {/* Overview */}
+
+        {/* Accounts Grid */}
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
