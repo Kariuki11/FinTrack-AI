61ca8ae4ee0d1013f11353a135bd3b72f769c8b2
Kariuki11 <irungukenneth433@gmail.com>
2025-01-17 02:07:02 +0300

    Updates on the layout.js file

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs"
-import {} from 'sonner'
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
           <main className="min-h-screen">
             {children}
           </main>
-          <Toaster/>
+          <Toaster richColors/>
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
