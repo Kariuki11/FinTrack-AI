39d0e245422bbb726aae0e207818457a1848345a
Kariuki11 <irungukenneth433@gmail.com>
2025-01-20 17:40:48 +0300

    New Updates

--- actions/accounts.js
+++ actions/accounts.js
@@ -68,6 +68,41 @@ export async function updateDefaultAccount(accountId) {
     }
 }
 
+export async function getAccountWithTransactions(accountId) {
+    const { userId } = await auth();
+        if (!userId) throw new Error("Not Authorized");
+
+        if (!accountId) {
+            throw new Error("Invalid accountId");
+        }
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+        if (!user) {
+            throw new Error("No User Found");
+        }
+
+        const account = await db.account.findUnique({
+            where: { id: accountId, userId: user.id },
+            include: {
+                transactions: {
+                    orderBy: {date: "desc"}
+                },
+                _count: {
+                    select: { transactions: true },
+                },
+            },
+        });
+
+        if (!account) return null;
+
+        return {
+            ...serializeTransaction(account),
+            transactions: account.transactions.map(serializeTransaction)
+        }
+}
+
 
 
 
