2f67554de022e50bd7ee1ea7f874d2b2acd20d9e
Kariuki11 <irungukenneth433@gmail.com>
2025-01-20 14:42:55 +0300

    New changes on actions dashboard.js file, page.jsx

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,27 +1,38 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
+import { ArrowDownRight, ArrowUpRight } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 const AccountCard = ({ account }) => {
     const { name, type, balance, id, isDefault } = account
   return (
     <div>
-        <Card>
-            <CardHeader>
-                <CardTitle>{name}</CardTitle>
-                <Switch />
-            </CardHeader>
-            <CardContent>
-                <div className="text-2xl font-bold">
-                    ${parseFloat(balance).toFixed(2)}
-                </div>
-                <p className="text-xs text-muted-foreground capitalize">
-                    {type.chaeAt(0) + type.slice(1).toLowerCase()} Account
-                </p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
+        <Card className="hover:shadow-md transition-shadow group relative">
+            <Link href={`/account/${id}`}>
+                <CardHeader >
+                    <CardTitle>{name}</CardTitle>
+                    <Switch />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        ${parseFloat(balance).toFixed(2)}
+                    </div>
+                    <p className="text-xs text-muted-foreground">
+                        {type.charAt(0) + type.slice(1).toLowerCase()} Account
+                    </p>
+                </CardContent>
+                <CardFooter>
+                    <div className="flex items-center">
+                        <ArrowUpRight className="mr-1 h-4 w-4 text-green-700"/>
+                        Income
+                    </div>
+                    <div className="flex items-center">
+                        <ArrowDownRight className="mr-1 h-4 w-4 text-red-600"/>
+                        Expense
+                    </div>
+                </CardFooter>
+            </Link>
         </Card>
 
     </div>
