2bcaf73cdd2e3cbfee68bc56d0a7fd071572c16f
Kariuki11 <irungukenneth433@gmail.com>
2025-01-20 17:49:54 +0300

    New Updates on the accounts section

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -13,13 +13,15 @@ const AccountsPage = async ({params}) => {
   return (
     <div className='space-y-8 px-5 flex gap-4 items-end justify-between'>
         <div>
-            <h1 className='text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize'>{account.name}</h1>
-            <p>{account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account</p>
+            <h1 className='text-5xl sm:text-4xl font-bold gradient-title capitalize'>{account.name}</h1>
+            <p className='text-muted-foreground'>{account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account</p>
         </div>
 
-        <div>
-            <div>${parseFloat(account.balance).toFixed(2)}</div>
-            <p>{account._count.transactions} transactions</p>
+        <div className='text-right pb-2'>
+            <div className='text-xl sm:text-2xl font-bold'>${parseFloat(account.balance).toFixed(2)}</div>
+            <p className='text-sm text-muted-foreground'>
+                {account._count.transactions} transactions
+            </p>
         </div>
     </div>
 
