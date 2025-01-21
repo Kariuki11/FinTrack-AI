812aa49715efffefa7670c66369fccc38929c7da
Kariuki11 <irungukenneth433@gmail.com>
2025-01-22 02:36:18 +0300

    New Updates created apis folder and added content in it

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Checkbox } from '@/components/ui/checkbox';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import React from 'react'
 
 const TransactionTable = ({ transactions }) => {
+
+    const handleSort = () => {};
+
   return (
     <div className='space-y-4'>
         {/* Filters */}
@@ -10,13 +16,32 @@ const TransactionTable = ({ transactions }) => {
         <div className="rounded-md border">
 
             <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Invoice</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Method</TableHead>
-                        <TableHead className="text-right">Amount</TableHead>
+                        <TableHead className="w-[50px]">
+                            <Checkbox/>
+                        </TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("date")}
+                        >
+                            <div className='flex items-center'>Date</div>
+                        </TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("category")}
+                        >
+                            <div className='flex items-center'>Category</div>
+                        </TableHead>
+                        <TableHead 
+                            className="cursor-pointer"
+                            onClick={() => handleSort("amount")}
+                        >
+                            <div className='flex items-center justify-end'>Amount</div>
+                        </TableHead>
+                        <TableHead>Recurring</TableHead>
+                        <TableHead className="w-[50px]" />
                     </TableRow>
                 </TableHeader>
                 <TableBody>
