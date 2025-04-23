67e1f144d77febb6d34df9a8269568bde85bbf45
Kariuki11 <irungukenneth433@gmail.com>
2025-04-23 13:22:51 +0300

    Add getCurrentBudget action to retrieve user's budget from the database

---
+++ actions/budget.js
@@ -0,0 +1,25 @@
+"use server";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getCurrentBudget(accountId) {
+    try {
+        const { userId } = await auth();
+        if (!userId) throw new Error("Not Authorized");
+        
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+        if (!user) {
+            throw new Error("No User Found");
+        }
+
+        const budget = await db.budget.findFirst({
+            where: {
+                //accountId: accountId,
+                userId: user.id,
+            },
+        });
+
+    } catch (error) {}
+}
